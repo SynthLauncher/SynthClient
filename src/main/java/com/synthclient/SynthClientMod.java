a854d17f373bb4525717538efb3d0fdf2a2d0974
jagath-sajjan <jagathsajjan097@gmail.com>
2025-05-10 18:41:03 +0530

    Add files via upload

---
+++ src/main/java/com/synthclient/SynthClientMod.java
@@ -0,0 +1,11 @@
+package com.synthclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SynthClientMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        // Initialization logic for SynthClient
+        System.out.println("SynthClientMod initialized!");
+    }
+}
